476fadf2ae0434f68832873363b8a1a42432ad1a
sstasishin <ctacik01@gmail.com>
2015-01-20 16:59:52 +0200

    'added_basic_classes'

--- src/js/core.js
+++ src/js/core.js
@@ -1,8 +1,7 @@
 
-
 class Core {
   constructor() {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 		function preload() {
 		}
@@ -11,7 +10,25 @@ class Core {
 		}
 
 		function update() {
+    }
+
+    function render() {
+      game.debug.inputInfo(32, 32);
 		}
 		
   }
+}
+
+class Ship {
+  constructor() {
+    this.batteryPower = 100;
+    this.protectShields = 100;
+  }  
+}
+
+class BattleShip extends Ship {
+  constructor() {
+    super();
+  }
+
 }
